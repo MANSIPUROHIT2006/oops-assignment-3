6aee74931b7b0f93950c06c789d1ff58fae69fac
MANSI2006 <mansipurohit18@gmail.com>
2025-05-06 07:23:14 +0530

    Create 62.cpp

---
+++ 62.cpp
@@ -0,0 +1,15 @@
+ // Implement a class Math with static function members for basic arithmetic operations. 
+#include <iostream>
+using namespace std;
+
+class Math {
+public:
+    
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+   
+    static int subtract(int a, int b) {
+        return a;
+    }
